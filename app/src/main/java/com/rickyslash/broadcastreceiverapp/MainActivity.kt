ee3a84a354f0e71d5987196cebaa646ae86e94b7
ricky-kiva <ricky.kiva@gmail.com>
2023-04-01 19:36:50 +0700

    add Main Actvt

--- app/src/main/java/com/rickyslash/broadcastreceiverapp/MainActivity.kt
+++ app/src/main/java/com/rickyslash/broadcastreceiverapp/MainActivity.kt
@@ -2,11 +2,48 @@ package com.rickyslash.broadcastreceiverapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
+import com.rickyslash.broadcastreceiverapp.databinding.ActivityMainBinding
+import android.Manifest
 
-class MainActivity : AppCompatActivity() {
+// note: need to add <uses-permission> & <receiver> inside the AndroidManifest.xml
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var binding: ActivityMainBinding? = null
+
+    // function for requesting permission for reading SMS
+    var requestPermissionLauncher = registerForActivityResult(
+        RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            Toast.makeText(this, "SMS Receiver Permission accepted", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "SMS Receiver Permission declined", Toast.LENGTH_SHORT)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.btnPermission?.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btn_permission -> requestPermissionLauncher.launch(Manifest.permission.RECEIVE_SMS)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
 
